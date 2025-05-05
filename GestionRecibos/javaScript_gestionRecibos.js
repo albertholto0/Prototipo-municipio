8246b854ca7f57ce5d8ad9501038d2b3b811c8df
Rafael Díaz López <kevinrdiaz98@gmail.com>
2025-05-05 16:20:54 -0600

    Se realizaron algunas modificaciones a los archivos de cobrar y gestion de recibos, fueron cuestiones de agregar cosas que faltaban, borrar cosas que estaban de más y cambiar algunas clases

---
+++ GestionRecibos/javaScript_gestionRecibos.js
@@ -0,0 +1,17 @@
+let isEditing = false;
+let recibos = [
+    {
+        folio: "0001",
+        fecha: "05/05/2025",
+        contribuyente: "Kevin Rafael Díaz López",
+        ejercicioFiscal: "2025",
+        ejercicioPeriodo: "Mayo-Junio",
+        descipción: "Pago de Agua",
+        total: "$120",
+        formaPago: "Efectivo",
+    },
+];
+
+const elements = {
+    folio: document.getElementById("folio"),
+};
