a6a12c24a803f066fa78bd7bcc35161d8eee3124
Albert Contreras <albertmendoza019@gmail.com>
2025-05-31 15:37:08 -0600

    Ahora el inicio de sesión usa los datos que se tengan en la tabla usuarios en la base de datos

--- models/gestionUsuariosModel.js
+++ models/gestionUsuariosModel.js
@@ -5,7 +5,7 @@ class Usuarios {
     static async getAll() {
         try {
             const [rows] = await db.query(
-                'SELECT nombres, apellido_paterno, apellido_materno, usuario, rol_usuario, fecha_acceso, hora_acceso, estado FROM usuarios');
+                'SELECT id_usuario, nombres, apellido_paterno, apellido_materno, usuario, password_usuario, rol_usuario, estado FROM usuarios');
             return rows;
         } catch (err) {
             console.error('Error en la consulta:', err);
