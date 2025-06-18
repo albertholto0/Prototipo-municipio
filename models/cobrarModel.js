8747d590c1a589a1dd3abb35421b41b901279e83
RafaelDiaz01 <rafaeldiazmx98@gmail.com>
2025-06-18 17:35:04 -0600

    En la clase Cobrar se agregaron los metódos static para obtener cuentas, subcuentas, secciones, conceptos y subconceptos. En esta parte se manejaron los SELECT para mostrar los niveles dependiendo del fk correspondiente a el nivel superior.

--- models/cobrarModel.js
+++ models/cobrarModel.js
@@ -1,17 +1,55 @@
 const db = require('../config/database');
 
 class Cobrar {
-    static async getContribuyente(){
+    static async getCuentas() {
         try {
             const [rows] = await db.query(`
-                SELECT
-                    nombre_completo
-                FROM contribuyentes
+                SELECT clave_cuenta_contable, nombre_cuentaContable FROM cuentas_contables
             `);
             return rows;
         } catch (err) {
             console.error('Error en la consulta:', err);
-            throw new Error('Error al obtener contribuyentes');
+            throw new Error('Error al obtener cuentas');
+        }
+    }
+
+    static async getSubcuentas(cuentaId) {
+        try {
+            const [rows] = await db.query('SELECT clave_subcuenta, nombre_subcuentas FROM subcuentas_contables WHERE clave_cuenta_contable = ?', [cuentaId]);
+            return rows;
+        } catch (err) {
+            console.error('Error en la consulta:', err);
+            throw new Error('Error al obtener subcuentas');
+        }
+    }
+
+    static async getSecciones(subcuentaId) {
+        try {
+            const [rows] = await db.query('SELECT clave_seccion, descripcion FROM secciones WHERE clave_subcuenta = ?', [subcuentaId]);
+            return rows;
+        } catch (err) {
+            console.error('Error en la consulta:', err);
+            throw new Error('Error al obtener secciones');
+        }
+    }
+
+    static async getConceptos(seccionId) {
+        try {
+            const [rows] = await db.query('SELECT clave_concepto, descripcion FROM conceptos WHERE clave_seccion = ?', [seccionId]);
+            return rows;
+        } catch (err) {
+            console.error('Error en la consulta:', err);
+            throw new Error('Error al obtener conceptos');
+        }
+    }
+
+    static async getSubconceptos(conceptoId) {
+        try {
+            const [rows] = await db.query('SELECT clave_subconcepto, descripcion FROM subconceptos WHERE clave_concepto = ?', [conceptoId]);
+            return rows;
+        } catch (err) {
+            console.error('Error en la consulta:', err);
+            throw new Error('Error al obtener subconceptos');
         }
     }
 }
