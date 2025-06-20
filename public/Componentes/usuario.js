378ce85249e8f4e432d23d5ec39075be9475875d
Albert Contreras <albertmendoza019@gmail.com>
2025-06-20 17:43:46 -0600

    Funciones para el usuario

--- public/Componentes/usuario.js
+++ public/Componentes/usuario.js
@@ -8,3 +8,9 @@ function esAdministrador() {
     const usuario = obtenerUsuarioActual();
     return usuario && usuario.rol_usuario && usuario.rol_usuario.toLowerCase().includes('admin');
 }
+
+// Funcion para obtener usuario y contraseña del usuario actual
+function obtenerCredenciales() {
+    const usuario = obtenerUsuarioActual();
+    return usuario ? { usuario: usuario.usuario, contraseña: usuario.password_usuario } : null;
+}
