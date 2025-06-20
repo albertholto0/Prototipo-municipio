46a01bd2a4f9dfb3ab94bcd29ddf5c51fab14eb5
EdenMCa <casarrubiaeden@gmail.com>
2025-06-19 18:01:52 -0600

    Se realizaron los archivos Route, controller y model para base catastral

---
+++ routes/gestionBaseCatastralRoute.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const gestionBaseCatastral = require('../models/gestionBaseCatastralModel');
+const { route } = require('./gestionContribuyenteRoute');
+const router = express.Router();
+
+// Ruta para obtener todos los registros de la base catastral   
+router.get('/', async (req, res) => {
+    try {
+        const registros = await gestionBaseCatastral.getAll();
+        res.json(registros);
+    } catch (error) {
+        console.error('Error al obtener registros de la base catastral:', error);
+        res.status(500).json({ message: 'Error al obtener registros de la base catastral' });
+    }
+});
