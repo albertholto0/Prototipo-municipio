d32dcf79c8a9a988420b5b905f21a97198ff8b75
RafaelDiaz01 <rafaeldiazmx98@gmail.com>
2025-06-20 16:09:12 -0600

    Se agregaron dos nuevos métodos, uno para obtener un contribuyente mediante su ID, y el otro para actualizar los datos del contribuyente. Adicionalmente se cambiaron los nombres de las columnas en el SELECT porque se modificó la base de datos.

--- models/gestionContribuyenteModel.js
+++ models/gestionContribuyenteModel.js
@@ -7,9 +7,13 @@ class Contribuyente {
       const [rows] = await db.query(`
         SELECT
           id_contribuyente,
-          nombre_completo,
+          nombre,
+          apellido_paterno,
+          apellido_materno,
+          fecha_nacimiento,
           rfc,
           direccion,
+          numero_calle,
           barrio,
           localidad,
           telefono
@@ -30,15 +34,41 @@ class Contribuyente {
     }
   }
 
-  static async setContribuyente(nombre_completo, fecha_nacimiento, telefono, direccion, barrio, localidad, codigo_postal, rfc) {
+  static async setContribuyente(nombre, apellido_paterno, apellido_materno, fecha_nacimiento, telefono, calle, num_calle, barrio, localidad, codigo_postal, rfc) {
     try {
       const [result] = await db.query(
-        'INSERT INTO contribuyente (nombre_completo, fecha_nacimiento, telefono, direccion, barrio, localidad, codigo_postal, rfc, copia_credencial) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)',
-        [nombre_completo, fecha_nacimiento, telefono, direccion, barrio, localidad, codigo_postal, rfc, "/home/imagenes/contribuyentes_ine"]
+        'INSERT INTO contribuyente (nombre, apellido_paterno, apellido_materno, fecha_nacimiento, telefono, direccion, numero_calle, barrio, localidad, codigo_postal, rfc, copia_credencial) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+        [nombre, apellido_paterno, apellido_materno, fecha_nacimiento, telefono, calle, num_calle, barrio, localidad, codigo_postal, rfc, "/home/imagenes/contribuyentes_ine"]
       );
       return result.insertId;
     } catch (err) {
       console.error('Error al crear usuario:', err);
+    }
+  }
+
+  static async getContribuyenteById(id) {
+    try {
+      const [rows] = await db.query('SELECT * FROM contribuyente WHERE id_contribuyente = ?', [id]);
+      if (rows.length === 0) {
+        throw new Error('Contribuyente no encontrado');
+      }
+      return rows[0];
+    } catch (err) {
+      console.error('Error al obtener el contribuyente:', err);
+      throw new Error('Error al obtener el contribuyente');
+    }
+  }
+
+  static async putContribuyente(id, nombre, apellido_paterno, apellido_materno, fecha_nacimiento, telefono, calle, num_calle, barrio, localidad, codigo_postal, rfc) {
+    try {
+      const [result] = await db.query(
+        'UPDATE contribuyente SET nombre = ?, apellido_paterno = ?, apellido_materno = ?, fecha_nacimiento = ?, telefono = ?, direccion = ?, numero_calle = ?, barrio = ?, localidad = ?, codigo_postal = ?, rfc = ? WHERE id_contribuyente = ?',
+        [nombre, apellido_paterno, apellido_materno, fecha_nacimiento, telefono, calle, num_calle, barrio, localidad, codigo_postal, rfc, id]
+      );
+      return result.affectedRows > 0;
+    } catch (err) {
+      console.error('Error al actualizar el contribuyente:', err);
+      throw new Error('Error al actualizar el contribuyente');
     }
   }
 }
