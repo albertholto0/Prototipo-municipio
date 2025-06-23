923969c9d05d726bd5411af479cdb75b0fab13d5
Rafael Díaz desde Laptop <rafaeldiazmx98@gmail.com>
2025-06-23 09:32:45 -0600

    En el archivo gestionSeccionRoutes.js se definen las rutas para la gestión de secciones contables dentro de una aplicación Express. Se implementaron rutas RESTful para obtener, registrar, buscar, eliminar y actualizar las secciones.

--- routes/gestionSeccionRoutes.js
+++ routes/gestionSeccionRoutes.js
@@ -5,7 +5,17 @@ const router = express.Router();
 // Ruta para obtener todas las secciones
 router.get('/', GestionSeccionController.getAllSecciones);
 
+// Registrar una nueva subcuenta contable
+router.post('/', GestionSeccionController.setSeccion);
+
 // Ruta para buscar una sección por clave
 router.get('/:clave_seccion', GestionSeccionController.getSeccionById);
+
+// Eliminar una subcuenta contable
+router.delete('/:clave_seccion', GestionSeccionController.deleteSeccion);
+
+// Actualizar una subcuenta contable
+router.put('/:clave_seccion', GestionSeccionController.putSeccion);
+
 
 module.exports = router;
