e4a9bf339a5cd0e2b50c538db4688b88a57d69f0
EdenMCa <casarrubiaeden@gmail.com>
2025-06-25 10:59:15 -0600

    Se realizo modificaciones para actualizar

--- models/gestionBaseCatastralModel.js
+++ models/gestionBaseCatastralModel.js
@@ -53,6 +53,33 @@ class BaseCatastral {
     }
   }
 
+  static async update(id, data) {
+  const { cuenta, base_catastral, id_contribuyente, ubicacion, barrio,
+    impuesto_calculado, fecha_avaluo, historial_avaluos } = data;
+
+  try {
+    const [result] = await db.query(
+      `UPDATE bases_catastrales SET 
+        cuenta = ?, 
+        base_catastral = ?, 
+        id_contribuyente = ?, 
+        ubicacion = ?, 
+        barrio = ?, 
+        impuesto_calculado = ?, 
+        fecha_avaluo = ?, 
+        historial_avaluos = ?
+      WHERE id_base_catastral = ?`,
+      [cuenta, base_catastral, id_contribuyente, ubicacion, barrio,
+        impuesto_calculado, fecha_avaluo, historial_avaluos, id]
+    );
+
+    return { id_base_catastral: id, ...data };
+  } catch (err) {
+    console.error('Error al actualizar base catastral:', err);
+    throw err;
+  }
+}
+
   static async delete(id) {
     await db.query(
       'DELETE FROM bases_catastrales WHERE id_base_catastral = ?',
